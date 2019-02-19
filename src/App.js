af0e4208c659c46af3159a0e3b95af80481ffd26
Ayodotun <dotunajala@gmail.com>
2019-02-19 13:38:34 +0100

    Todo component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './component/Todos'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Pray for at least an hour",
+        is_completed: false
+      },
+      {
+        id: 2,
+        title: "Brush you teeth",
+        is_completed: false
+      },
+      {
+        id: 3,
+        title: "Read your bible for at least an hour",
+        is_completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>Todos</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
